1be4e64ec62ecc0bb1af66d4614fe76abc4fbe8b
OseiAlex <34588600+OseiAlex@users.noreply.github.com>
2023-07-06 14:47:15 +0000

    Modified mainscreen.dart

--- lib/AllScreens/mainscreen.dart
+++ lib/AllScreens/mainscreen.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 class MainScreen extends StatelessWidget {
   static const String idScreen = "mainScreen";
 
-  const MainScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
